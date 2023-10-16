fa546347cb35b929277f3668c778e0ed2738c29b
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2023-10-16 07:40:53 +0300

    Update Server.cpp

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -1,86 +1,72 @@
-﻿#include <iostream>
-
+#include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 using namespace std;
 
-int main()
-{
-    // Initialize WSA
+int main() {
     WSADATA wsaData;
     int wsa_startup_return_code;
     WORD wVersion = MAKEWORD(2, 2);
 
     wsa_startup_return_code = WSAStartup(wVersion, &wsaData);
-    if (wsa_startup_return_code == 0) {
-        cout << "The Winsock dll found!" << endl;
-        cout << "The status: " << wsaData.szSystemStatus << endl;
-    }
-    else {
-        cout << "The Winsock dll found!" << endl;
-        return 0;
+    if (wsa_startup_return_code != 0) {
+        cout << "WSAStartup failed with error code: " << wsa_startup_return_code << endl;
+        return 1;
     }
 
-
-    // Create a socket
     SOCKET serverSocket, acceptSocket;
-
     serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (serverSocket == INVALID_SOCKET) {
         cout << "Error at socket(): " << WSAGetLastError() << endl;
         WSACleanup();
-        return 0;
-    }
-    else {
-        cout << "socket() is OK!" << endl;
+        return 1;
     }
 
-
-    // Bind the socket with port
-    int port = 55555;
     sockaddr_in service;
+    int port = 55555;
     service.sin_family = AF_INET;
-    
     InetPton(AF_INET, L"127.0.0.1", &service.sin_addr.s_addr);
     service.sin_port = htons(port);
+
     if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
         cout << "Binding failed: " << WSAGetLastError() << endl;
         closesocket(serverSocket);
         WSACleanup();
-        return 0;
+        return 1;
     }
-    cout << "bind() is OK!" << endl;
 
-    // Listen on the socket
     if (listen(serverSocket, 1) == SOCKET_ERROR) {
         cout << "Error listening on socket: " << WSAGetLastError() << endl;
-        return 0;
-    }
-    else {
-        cout << "Start listening on port: " << port << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
     }
 
-    // Accept a connection
+    cout << "Start listening on port: " << port << endl;
+
     acceptSocket = accept(serverSocket, NULL, NULL);
     if (acceptSocket == INVALID_SOCKET) {
         cout << "Accept failed: " << WSAGetLastError() << endl;
+        closesocket(serverSocket);
         WSACleanup();
-        return -1;
-    }
-    else {
-        cout << "Accepted connection" << endl;
-        //system("pause");
-        //WSACleanup();
+        return 1;
     }
 
-    // Receive data
+    cout << "Accepted connection" << endl;
+
     char receiveBuffer[200] = "";
     int byteCount = recv(acceptSocket, receiveBuffer, 200, 0);
     if (byteCount < 0) {
-        cout << "Server error!" << WSAGetLastError() << endl;
-        return -1;
+        cout << "Server error: " << WSAGetLastError() << endl;
+    } else {
+        cout << "Received data: " << receiveBuffer << endl;
     }
 
-    cout << receiveBuffer;
+    // Properly close the sockets and clean up Winsock
+    closesocket(acceptSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
 }

94d48e9ead51bea08624920b9add334f8a05b908
User#None <77580844+issamansur@users.noreply.github.com>
2023-11-06 00:43:46 +0200

    Fix type to name of var in `free()`

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -131,7 +131,7 @@ int main(int argc, char** argv) {
             }
             cout << "Client sent header" << endl;
 
-            free(tm);
+            free(now);
             free(timeString);
         }
 
